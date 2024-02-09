7a39571453c8720159ed4388e838b9fea868172f
kaldyr <kaldyr@gmail.com>
2024-02-08 23:36:59 -0800

    simplifying

--- flake.nix
+++ flake.nix
@@ -13,81 +13,72 @@
   flake-utils.lib.eachDefaultSystem (system:
   let
 
-    myNeovimOverlay = prev: final:
-		let
-			myNeovimUnwrapped = prev.wrapNeovim prev.neovim {
-				configure = {
-					customRC = ''
-						set runtimepath+=${./config}
-						source ${./config/init.lua}
-					'';
-					packages.all.start = with pkgs.vimPlugins; [
-						catppuccin-nvim
-						cmp-buffer
-						cmp-cmdline
-						cmp-nvim-lsp
-						cmp-path
-						cmp_luasnip
-						comment-nvim
-						fidget-nvim
-						flash-nvim
-						friendly-snippets
-						gitsigns-nvim
-						indent-blankline-nvim
-						lualine-nvim
-						luasnip
-						neodev-nvim
-						neoscroll-nvim
-						noice-nvim
-						nui-nvim
-						nvim-cmp
-						nvim-colorizer-lua
-						nvim-lspconfig
-						nvim-tree-lua
-						(nvim-treesitter.withPlugins (_: nvim-treesitter.allGrammars))
-						nvim-treesitter-context
-						nvim-treesitter-textobjects
-						nvim-ts-autotag
-						nvim-ufo
-						nvim-web-devicons
-						obsidian-nvim
-						plenary-nvim
-						promise-async
-						rainbow-delimiters-nvim
-						sort-nvim
-						telescope-frecency-nvim
-						telescope-fzf-native-nvim
-						telescope-nvim
-						telescope-symbols-nvim
-						telescope-ui-select-nvim
-						vim-fugitive
-						vim-illuminate
-						which-key-nvim
-					];
-				};
+		neovimWrapped = pkgs.wrapNeovim pkgs.neovim {
+			configure = {
+				customRC = ''
+					set runtimepath+=${./config}
+					source ${./config/init.lua}
+				'';
+				packages.all.start = with pkgs.vimPlugins; [
+					catppuccin-nvim
+					cmp-buffer
+					cmp-cmdline
+					cmp-nvim-lsp
+					cmp-path
+					cmp_luasnip
+					comment-nvim
+					fidget-nvim
+					flash-nvim
+					friendly-snippets
+					gitsigns-nvim
+					indent-blankline-nvim
+					lualine-nvim
+					luasnip
+					neodev-nvim
+					neoscroll-nvim
+					noice-nvim
+					nui-nvim
+					nvim-cmp
+					nvim-colorizer-lua
+					nvim-lspconfig
+					nvim-tree-lua
+					(nvim-treesitter.withPlugins (_: nvim-treesitter.allGrammars))
+					nvim-treesitter-context
+					nvim-treesitter-textobjects
+					nvim-ts-autotag
+					nvim-ufo
+					nvim-web-devicons
+					obsidian-nvim
+					plenary-nvim
+					promise-async
+					rainbow-delimiters-nvim
+					sort-nvim
+					telescope-frecency-nvim
+					telescope-fzf-native-nvim
+					telescope-nvim
+					telescope-symbols-nvim
+					telescope-ui-select-nvim
+					vim-fugitive
+					vim-illuminate
+					which-key-nvim
+				];
 			};
-		in 
-		{
-			myNeovim = prev.writeShellApplication {
-        name = "neovim";
-        text = ''
-          ${myNeovimUnwrapped}/bin/nvim "$@"
-        '';
-      };
-    };
+		};
 
     pkgs = import nixpkgs {
       system = system;
-      overlays = [
-				(final: prev: { neovim = neovim.packages.${prev.system}.neovim; })
-        myNeovimOverlay
-      ];
+      overlays = [ (final: prev: { neovim = neovim.packages.${prev.system}.neovim; }) ];
     };
 
   in
   {
     packages = rec {
-      nvim = pkgs.myNeovim;
+      nvim = pkgs.writeShellApplication {
+        name = "neovim";
+        text = ''
+          ${neovimWrapped}/bin/nvim "$@"
+        '';
+      };
       default = nvim;
     };
 
