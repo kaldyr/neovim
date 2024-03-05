06caddad49ecbdb017c7ca99c4cf7ec8e73ae0eb
kaldyr <kaldyr@gmail.com>
2024-03-05 01:24:11 -0800

    formatting

--- flake.nix
+++ flake.nix
@@ -18,58 +18,58 @@
       overlays = [ (final: prev: { neovim = neovim.packages.${prev.system}.neovim; }) ];
     };
 
-		neovimWrapped = pkgs.wrapNeovim pkgs.neovim {
-			configure = {
-				customRC = ''
-					set runtimepath+=${./config}
-					source ${./config/init.lua}
-				'';
-				packages.all.start = with pkgs.vimPlugins; [
-					boole-nvim
-					catppuccin-nvim
-					cmp-buffer
-					cmp-cmdline
-					cmp-nvim-lsp
-					cmp-path
-					cmp_luasnip
-					comment-nvim
-					fidget-nvim
-					flash-nvim
-					friendly-snippets
-					gitsigns-nvim
-					indent-blankline-nvim
-					lualine-nvim
-					luasnip
-					neodev-nvim
-					neoscroll-nvim
-					noice-nvim
-					nui-nvim
-					nvim-cmp
-					nvim-colorizer-lua
-					nvim-lspconfig
-					nvim-tree-lua
-					nvim-treesitter.withAllGrammars
-					nvim-treesitter-context
-					nvim-treesitter-textobjects
-					nvim-ts-autotag
-					nvim-ufo
-					nvim-web-devicons
-					obsidian-nvim
-					plenary-nvim
-					promise-async
-					rainbow-delimiters-nvim
-					sort-nvim
-					telescope-frecency-nvim
-					telescope-fzf-native-nvim
-					telescope-nvim
-					telescope-symbols-nvim
-					telescope-ui-select-nvim
-					vim-fugitive
-					vim-illuminate
-					which-key-nvim
-				];
-			};
-		};
+    neovimWrapped = pkgs.wrapNeovim pkgs.neovim {
+      configure = {
+        customRC = ''
+          set runtimepath+=${./config}
+          source ${./config/init.lua}
+        '';
+        packages.all.start = with pkgs.vimPlugins; [
+          boole-nvim
+          catppuccin-nvim
+          cmp-buffer
+          cmp-cmdline
+          cmp-nvim-lsp
+          cmp-path
+          cmp_luasnip
+          comment-nvim
+          fidget-nvim
+          flash-nvim
+          friendly-snippets
+          gitsigns-nvim
+          indent-blankline-nvim
+          lualine-nvim
+          luasnip
+          neodev-nvim
+          neoscroll-nvim
+          noice-nvim
+          nui-nvim
+          nvim-cmp
+          nvim-colorizer-lua
+          nvim-lspconfig
+          nvim-tree-lua
+          nvim-treesitter.withAllGrammars
+          nvim-treesitter-context
+          nvim-treesitter-textobjects
+          nvim-ts-autotag
+          nvim-ufo
+          nvim-web-devicons
+          obsidian-nvim
+          plenary-nvim
+          promise-async
+          rainbow-delimiters-nvim
+          sort-nvim
+          telescope-frecency-nvim
+          telescope-fzf-native-nvim
+          telescope-nvim
+          telescope-symbols-nvim
+          telescope-ui-select-nvim
+          vim-fugitive
+          vim-illuminate
+          which-key-nvim
+        ];
+      };
+    };
 
     runtimeDeps = pkgs.symlinkJoin {
       name = "runtimeDeps";
