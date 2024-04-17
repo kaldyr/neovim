bf50d1eca825b977d1e9805b6f99a76392ffb21b
kaldyr <kaldyr@gmail.com>
2024-04-17 14:41:42 -0700

    lspconfig setup emmet_ls and a-h/templ.  Flake update.

--- config/lua/plugins/nvim-lspconfig.lua
+++ config/lua/plugins/nvim-lspconfig.lua
@@ -21,39 +21,43 @@ if status_ok then
 end
 
 local on_attach = function(_, bufnr)
-
-    local nmap = function(keys, func, desc)
-    if desc then
-        desc = 'LSP: ' .. desc
-    end
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-    end
-
-    nmap('gd', vim.lsp.buf.definition, 'Goto Definition')
-    nmap('gD', vim.lsp.buf.declaration, 'Goto Declaration')
-    nmap('gI', vim.lsp.buf.implementation, 'Goto Implementation')
-    nmap('gr', vim.lsp.buf.references, 'Goto References')
-    nmap('gT', vim.lsp.buf.type_definition, 'Goto Type Definition')
-    nmap('<leader>ch', vim.lsp.buf.hover, 'Hover Documenation')
-    nmap('<leader>cs', vim.lsp.buf.signature_help, 'Signature Documenation')
-    nmap('<leader>ca', vim.buf.code_action, 'Code Action')
-    nmap('<leader>cr', vim.buf.rename, 'Code Rename')
-    nmap('<leader>ds', vim.buf.document_symbols, 'Document Symbols')
-    nmap('<leader>wa', vim.buf.add_workspace, 'Workspace Add Folder')
-    nmap('<leader>wl', vim.buf.list_workspace, 'Workspace List Folders')
-    nmap('<leader>wr', vim.buf.remove_workspace, 'Workspace Remove Folder')
-    nmap('<leader>ws', vim.buf.workspace_symbols, 'Workspace Symbols')
-
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition,  { buffer = bufnr, desc = 'Goto Definition' })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr, desc = 'Goto Declaration' })
+    vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, { buffer = bufnr, desc = 'Goto Implementation' })
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = bufnr, desc = 'Goto References' })
+    vim.keymap.set('n', 'gT', vim.lsp.buf.type_definition, { buffer = bufnr, desc = 'Goto Type Definition' })
+    vim.keymap.set('n', '<leader>ch', vim.lsp.buf.hover, { buffer = bufnr, desc = 'Hover Documenation' })
+    vim.keymap.set('n', '<leader>cs', vim.lsp.buf.signature_help, { buffer = bufnr, desc = 'Signature Documenation' })
+    vim.keymap.set('n', '<leader>ca', vim.buf.code_action, { buffer = bufnr, desc = 'Code Action' })
+    vim.keymap.set('n', '<leader>cr', vim.buf.rename, { buffer = bufnr, desc = 'Code Rename' })
+    vim.keymap.set('n', '<leader>ds', vim.buf.document_symbols, { buffer = bufnr, desc = 'Document Symbols' })
+    vim.keymap.set('n', '<leader>wa', vim.buf.add_workspace, { buffer = bufnr, desc = 'Workspace Add Folder' })
+    vim.keymap.set('n', '<leader>wl', vim.buf.list_workspace, { buffer = bufnr, desc = 'Workspace List Folders' })
+    vim.keymap.set('n', '<leader>wr', vim.buf.remove_workspace, { buffer = bufnr, desc = 'Workspace Remove Folder' })
+    vim.keymap.set('n', '<leader>ws', vim.buf.workspace_symbols, { buffer = bufnr, desc = 'Workspace Symbols' })
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
+vim.filetype.add({ extension = { templ = "templ" } }) -- a-h/templ Go
+
 lspconfig.cssls.setup { on_attach = on_attach, capabilities = capabilities }
-lspconfig.emmet_ls.setup { on_attach = on_attach, capabilities = capabilities }
+lspconfig.emmet_ls.setup({
+    -- on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "css", "html", "templ" },
+    init_options = {
+        html = {
+            options = {
+                ["bem.enabled"] = true,
+            },
+        },
+    }
+})
 lspconfig.gopls.setup { on_attach = on_attach, capabilities = capabilities }
-lspconfig.html.setup { on_attach = on_attach, capabilities = capabilities }
-lspconfig.htmx.setup { on_attach = on_attach, capabilities = capabilities }
+lspconfig.html.setup { on_attach = on_attach, capabilities = capabilities , filetypes = { "html", "templ" } }
+lspconfig.htmx.setup { on_attach = on_attach, capabilities = capabilities, filetypes = { "html", "templ" } }
 lspconfig.lua_ls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
@@ -66,6 +70,7 @@ lspconfig.lua_ls.setup {
 }
 lspconfig.marksman.setup { on_attach = on_attach, capabilities = capabilities }
 lspconfig.nil_ls.setup { on_attach = on_attach, capabilities = capabilities }
+lspconfig.templ.setup { on_attach = on_attach, capabilities = capabilities }
 
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
