a4b7e286ed0d1689b2e504d012b5b31f28121379
kaldyr <kaldyr@gmail.com>
2024-11-15 13:54:10 -0800

    Open telescope instead of netrw if neovim opened a folder

--- config/lua/autocmd.lua
+++ config/lua/autocmd.lua
@@ -2,11 +2,26 @@
 vim.api.nvim_create_autocmd ( 'VimEnter', {
     pattern = '*',
     callback = function()
+
         -- If neovim is passed a folder instead of a file, set the working directory to the folder
         local pwd = vim.fn.expand('%:p:h')
         vim.api.nvim_set_current_dir(pwd)
+
         -- Turn on list characters, can be toggled by :set list!
         vim.cmd [[ set list ]]
+
+        -- Set neovim's working directory root to the folder the opened file is in.
+        vim.api.nvim_set_current_dir( vim.fn.expand('%:p:h') )
+
+        -- If neovim opened a folder instead of a file, remove netrw and open telescope find_files
+        if vim.fn.expand('%:p:h') == string.sub( vim.fn.expand('%p'), 0, -1 ) then
+            vim.api.nvim_buf_delete( 0, { force = true } )
+            local status_ok, telescope = pcall( require, 'telescope.builtin' )
+            if status_ok then
+                telescope.find_files()
+            end
+        end
+
     end,
 })
 
